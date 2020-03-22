f00ddffa1cabb8a9a2098ccdaf5729ea1b11a6b3
183754539@qq.com <183754539@qq.com>
2020-03-22 20:09:04 +0800

    Default Changelist

--- tool.py
+++ tool.py
@@ -7,6 +7,9 @@
 # @Software: PyCharm
 
 import requests
+import numpy
+import sys
+import easygui as ui
 import pandas as pd
 from lxml import etree
 
@@ -17,6 +20,7 @@ def requests_info(website):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3941.4 Safari/537.36"}
     responses = requests.get(url=url, headers=headers).content.decode('utf-8')
     return responses
+
 
 class do_somting():
     def __init__(self, web_site):
@@ -33,7 +37,8 @@ class do_somting():
         base_info_4 = self.tree.xpath('//div/div[3]/div/span/text()')
         base_info_5 = self.tree.xpath('//div/div[3]/div/text()')[2]
         base_info = base_info_1 + str(base_info_2[0]) + base_info_3 + str(base_info_4[0]) + base_info_5
-        print("[√]完成：" + base_info)
+        ui.msgbox(msg=base_info.replace(" ", "").replace("\n", ""), title="网站" + self.web_site + "扫描结果")
+        # print("[√]完成：" + base_info)
 
     def get_table(self):
         self.tree = etree.HTML(requests_info(self.web_site))
@@ -52,6 +57,7 @@ class do_somting():
             tips.append(cms[0])
             huanjing = tr.xpath("td[6]/text()")
             tips.append(huanjing[0])
+            # ui.textbox(msg="网站" + website + "同IP网站的详细信息", title="网站" + website + "同IP网站的详细信息", text=tips)
             all_info.append(tips)
             print(tips)
 
@@ -68,27 +74,42 @@ class do_somting():
             self.web_site = temp
 
     def savexls(self):
-        print("[!]正在保存结果至"+website+"结果.xls")
+        output = open('data.txt', 'w', encoding='gbk')
+        output.write('url,status,title,cms,huanjing\n')
+        for row in all_info:
+            rowtxt = '{},{},{},{}'.format(row[0], row[1], row[2], row[3])
+            output.write(rowtxt + "\n")
+        output.close()
+        with open("data.txt", encoding="gbk") as rp:
+            text = rp.read()
+        ui.textbox(msg="网站" + website + "同IP网站的详细信息", title="网站" + website + "同IP网站的详细信息", text=text)
+        weizhi = ui.fileopenbox(msg="表格保存", title="表格保存", default="./" + website + "结果.xls", filetypes="xls")
+        print("[!]正在保存结果至" + website + "结果.xls")
         data_pd = pd.DataFrame(data=all_info, columns=["url", "状态值", "网站标题", "可能的CMS", "网站环境(包括中间件、开发框架、语言等信息)"])
         try:
-            data_pd.to_excel(website+"结果.xls")
+            data_pd.to_excel(weizhi)
         except IOError:
+            ui.msgbox("[x]保存失败，请检查是否已经打开该文件，或本程序是否有本文件夹及文件的读写权限")
             print("[x]保存失败，请检查是否已经打开该文件，或本程序是否有本文件夹及文件的读写权限")
         else:
+            ui.msgbox("[√]保存成功！")
             print("[√]保存成功")
-
 
 
 # 临时代码测试
 
 # main()
 if __name__ == '__main__':
-    all_info = []
-    website = input("[!]请输入网址，如www.site.com：\n")
-    # website = "www.sdyunet.cn"  # 测试
-    do_somthing_init = do_somting(web_site=website)
-    do_somthing_init.getinfo()
-    do_somthing_init.get_table()
-    do_somthing_init.page_num()
-    do_somthing_init.savexls()
 
+    if ui.ccbox(msg="本软件仅供测试使用，对使用过程中造成的一切后果，作者不承担任何责任。", title="免责声明", choices=["同意承担使用本软件造成的一切责任", "算了吧，退出"]):
+        website = ui.enterbox(msg="请输入网址，如:www.sdyunet.cn", title="同IP网站信息检测", default="www.sdyunet.cn")
+        # print(website)
+        all_info = []
+        # website = input("[!]请输入网址，如www.site.com：\n")
+        do_somthing_init = do_somting(web_site=website)
+        do_somthing_init.getinfo()
+        do_somthing_init.get_table()
+        do_somthing_init.page_num()
+        do_somthing_init.savexls()
+    else:
+        sys.exit(0)
